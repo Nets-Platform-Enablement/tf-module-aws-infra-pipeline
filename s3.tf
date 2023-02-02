afb6fc012d6d94ee6f668357c1aa5159d96e47ae
Juha Leinonen <juha.leinonen@nexigroup.com>
2023-02-02 08:51:11 +0200

    Adding s3 event notifications

--- s3.tf
+++ s3.tf
@@ -90,3 +90,12 @@ data "aws_iam_policy_document" "allow_ssl_requests_only" {
     }
   }
 }
+
+# S3 Event notificactions for bucket
+resource "aws_s3_bucket_notification" "artifact_store_bucket_notificaction" {
+  bucket = aws_s3_bucket.codepipeline_artifacts_store.id
+  topic {
+    topic_arn     = module.sns_topic.sns_topic_arn
+    events        = ["s3:ObjectRemoved:*"] # Permanently deleted, Delete marker created
+  }
+}
