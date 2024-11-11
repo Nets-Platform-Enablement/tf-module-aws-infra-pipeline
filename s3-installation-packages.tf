561fc01290dd5b997cc32293a692d43cddbefb37
Joona Somerkivi <joona.somerkivi@nexigroup.com>
2024-11-11 13:42:28 +0200

    WIP: downloading installation packages to S3

---
+++ s3-installation-packages.tf
@@ -0,0 +1,72 @@
+resource "aws_s3_bucket" "packages" {
+  bucket        = lower("${local.name}-packages-${var.environment}")
+  tags          = local.tags
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "packages" {
+  bucket = aws_s3_bucket.packages.bucket
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "packages" {
+  bucket     = aws_s3_bucket.packages.bucket
+  acl        = "private"
+  depends_on = [aws_s3_bucket_ownership_controls.owner]
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "packages" {
+  bucket = aws_s3_bucket.packages.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+    bucket_key_enabled = false
+  }
+}
+
+resource "aws_s3_bucket_versioning" "packages" {
+  bucket = aws_s3_bucket.packages.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+# Installation packages
+################
+locals {
+  packages = {
+    terraform = {
+      target = "terraform-${var.terraform_version}.zip"
+      source = "https://releases.hashicorp.com/terraform/${var.terraform_version}/terraform_${var.terraform_version}_linux_amd64.zip"
+    }
+  }
+}
+resource "null_resource" "download_package" {
+  for_each = local.packages
+	
+  provisioner "local-exec" {
+    command = <<EOF
+    curl -qL -o /tmp/${each.value.target} ${each.value.source}
+    EOF
+  }
+
+  triggers = {
+    terget = each.value.target
+    source = each.value.source
+  }
+} 
+
+resource "aws_s3_bucket_object" "packages" {
+  for_each  = local.packages
+  bucket    = aws_s3_bucket.packages.bucket
+  key       = each.value.target
+  source    = "/tmp/${each.value.target}"
+
+  depends_on = [ null_resource.download_package ]
+}
