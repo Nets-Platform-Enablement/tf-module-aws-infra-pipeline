debe1dc11300e4d5c95d7d3a378d7a931478d8e3
Joona Somerkivi <joona.somerkivi@nexigroup.com>
2024-11-11 15:36:55 +0200

    wip: testing downloading from s3

--- code-build.tf
+++ code-build.tf
@@ -35,7 +35,8 @@ resource "aws_codebuild_project" "tflint" {
     buildspec = templatefile(
       "${path.module}/files/buildspec_tflint.yml.tftpl",
       {
-        TF_SOURCE = "${aws_s3_bucket.packages.bucket_regional_domain_name}/${local.packages.terraform.target}",
+        #TF_SOURCE = "${aws_s3_bucket.packages.bucket_regional_domain_name}/${local.packages.terraform.target}",
+        TF_SOURCE = "${aws_s3_bucket.packages.bucket}/${local.packages.terraform.target}",
         DIRECTORY  = var.directory,
         BACKENDFILE = var.tfbackend_file,
         
