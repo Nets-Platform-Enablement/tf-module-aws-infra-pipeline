a4088f9468354fa84bd0ee9512f0d1b0f9b8f6ec
Juha Leinonen <julei@nets.eu>
2025-09-03 11:38:17 +0300

    Add validation to name to be max 38 characters as name is used as prefix

--- variables.tf
+++ variables.tf
@@ -35,6 +35,10 @@ variable "name" {
     condition     = length(var.name) == 0 || can(regex("^[0-9A-Za-z_-]+$", var.name))
     error_message = "For the name value only a-Z, 0-9, dash and underscore are allowed."
   }
+  validation {
+    condition     = length(var.name) == 0 || length(var.name) <= 38
+    error_message = "The name value must be 38 characters or less."
+  }
 }
 
 variable "require_manual_approval" {
